39671bf442f799fec943fd9cfdeaf83bb5b2f8fe
Neven Leung <nevyn.leong@gmail.com>
2018-06-03 02:45:11 +0800

    [Updated] the domOperationModule. [Added] polyfill for el.closest().

--- scripts/app.js
+++ scripts/app.js
@@ -34,10 +34,29 @@ const data = {
 
 // ------------------------- modules --------------------------------
 
+// polyfill
+(function () {
+  //closest- https://developer.mozilla.org/zh-CN/docs/Web/API/Element/closest
+  if (!Element.prototype.matches)
+    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+      Element.prototype.webkitMatchesSelector;
+
+  if (!Element.prototype.closest)
+    Element.prototype.closest = function (s) {
+      let el = this;
+      if (!document.documentElement.contains(el)) return null;
+      do {
+        if (el.matches(s)) return el;
+        el = el.parentElement;
+      } while (el !== null);
+      return null;
+    };
+}());
+
 /**
  * domOperationModule  将常用的DOM操作进行封装
  *
- * @return {object} {appendMultiChild, hasClass, query, queryAll, findClosestAncestor, findSiblingForward, findSiblingBackward}
+ * @return {object} {appendMultiChild, hasClass, query, queryAll, findClosestAncestor, findSibling, findSiblings}
  *
  ******************************************************************************************
  * appendMultiChild()  将多个节点按顺序添加到parentNode，作为其子节点
@@ -48,136 +67,238 @@ const data = {
  * hasClass()  判断某个元素是否含有相应的className
  *
  * @param $el  需要判断的元素
- * @param {string} className  单个class名称
+ * @param className {string} 单个class名称
  ******************************************************************************************
  * query()  基于$el去查找第一个符合selector的元素
  *
  * @param $el  基准元素
- * @param {string} selector  合法的css选择器字符串
+ * @param selector {string} 合法的css选择器字符串
  ******************************************************************************************
  * queryAll()  基于$el去查找符合selector的元素集合
  *
  * @param $el  基准元素
- * @param {string} selector  合法的css选择器字符串
+ * @param selector {string} 合法的css选择器字符串
  ******************************************************************************************
- * findClosestAncestor()  寻找第一个拥有相应className的祖先节点
+ * findClosestAncestor()  寻找第一个符合selector的祖先节点
  *
  * @param $el  开始寻找的基准元素
- * @param {string} selector  单个class名称
+ * @param selector {string} 合法的css选择器字符串
  ******************************************************************************************
- * findSiblingForward()  向前寻找第一个拥有相应className的兄弟元素
+ * findSibling()  寻找第一个符合selector的兄弟元素
  *
  * @param $el  开始寻找的基准元素
- * @param {string} className  单个class名称
+ * @param selector {string} 合法的css选择器字符串
+ * @param option {string}  查找选项 forward(default)|backward
  ******************************************************************************************
- * findSiblingBackward()  向后寻找第一个拥有相应className的兄弟元素
+ * findSiblings()  寻找符合selector的兄弟元素集合(排除$el本身)
  *
  * @param $el  开始寻找的基准元素
- * @param {string} className  单个class名称
+ * @param selector {string} 合法的css选择器字符串
+ * @param option {string}  查找选项 all(default)|forward|backward
  ******************************************************************************************
  */
 
 const domOperationModule = (function () {
+  // 将多个节点按顺序添加到parentNode，作为其子节点
   function appendMultiChild(parentNode, ...childrenNodes) {
-    if(typeof parentNode !== 'undefined' && typeof childrenNodes !== 'undefined') {
-      childrenNodes.forEach((childNode) => {
-        parentNode.appendChild(childNode);
-      })
-    } else {
-      console.log('ParentNode or childrenNodes is not defined.');
+    try {
+      if (parentNode instanceof Node && childrenNodes.length !== 0) {
+        childrenNodes.forEach((childNode) => {
+          if (childNode instanceof Node) {
+            parentNode.appendChild(childNode);
+          } else {
+            throw new Error(`${childNode} is not of type 'Node'`);
+          }
+        })
+      } else {
+        console.log('ParentNode or childrenNodes is not defined.');
+      }
+    }
+    catch (ex) {
+      console.error(ex);
     }
   }
 
+  // 判断某个元素是否含有相应的className
   function hasClass($el, className) {
     if ('classList' in $el) {
       return $el.classList.contains(className);
     }
   }
 
+  // 基于$el去查找第一个符合selector的元素
   // 将document.querySelector与element.querySelector区分开，对后者作了一些改进，基于element向子代元素查找，使得查找的结果符合预期
   function query($el, selector) {
-    if (typeof $el !== 'undefined' && typeof selector !== 'undefined') {
-      if ($el === document) {
-        return $el.querySelector(selector);
-      } else {
-        const originID = $el.getAttribute('id');
-        const newID = originID || 'temp';
+    try {
+      if ($el instanceof Node && typeof selector === 'string') {
+        if ($el instanceof Document) {
+          return $el.querySelector(selector);
+        } else {
+          const originID = $el.getAttribute('id');
+          const newID = originID || 'temp';
 
-        $el.setAttribute('id', newID);
-        selector = `#${newID} ${selector}`;
+          $el.setAttribute('id', newID);
+          selector = `#${newID} ${selector}`;
 
-        const result = $el.querySelector(selector);
+          const result = $el.querySelector(selector);
 
-        // 移除临时的id属性
-        if (!originID) {
-          $el.removeAttribute('id');
+          // 移除临时的id属性
+          if (!originID) {
+            $el.removeAttribute('id');
+          }
+
+          return result;
         }
-
-        return result;
       }
+    }
+    catch (ex) {
+      console.error(ex);
     }
   }
 
+  // 基于$el去查找符合selector的元素集合
   // 将document.querySelectorAll与element.querySelectorAll区分开，对后者作了一些改进，基于element向子代元素查找，使得查找的结果符合预期
   function queryAll($el, selector) {
-    if (typeof $el !== 'undefined' && typeof selector !== 'undefined') {
-      if ($el === document) {
-        return $el.querySelectorAll(selector);
-      } else {
-        const originID = $el.getAttribute('id');
-        const newID = originID || 'temp';
+    try {
+      if ($el instanceof Node && typeof selector === 'string') {
+        if ($el instanceof Document) {
+          return $el.querySelectorAll(selector);
+        } else {
+          const originID = $el.getAttribute('id');
+          const newID = originID || 'temp';
 
-        $el.setAttribute('id', newID);
-        selector = `#${newID} ${selector}`;
+          $el.setAttribute('id', newID);
+          selector = `#${newID} ${selector}`;
 
-        const result = $el.querySelectorAll(selector);
+          const result = $el.querySelectorAll(selector);
 
-        // 移除临时的id属性
-        if (!originID) {
-          $el.removeAttribute('id');
+          // 移除临时的id属性
+          if (!originID) {
+            $el.removeAttribute('id');
+          }
+
+          return result;
         }
-
-        return result;
       }
+    }
+    catch (ex) {
+      console.error(ex);
     }
   }
 
+  // 寻找第一个符合selector的祖先节点
   // $el.closest()会返回自身，这并不符合实际的使用需求，以下代码对这一点作了特殊处理
   function findClosestAncestor($el, selector) {
-    if (typeof $el !== 'undefined' && typeof selector !== 'undefined') {
-      if ($el.closest(selector) === $el) {
-        if ($el.parentElement && $el.parentElement !== document) {
-          return $el.parentElement.closest(selector);
+    try {
+      if ($el instanceof Element && typeof selector === 'string') {
+        if ($el.closest(selector) === $el) {
+          if ($el.parentElement instanceof Element) {
+            return $el.parentElement.closest(selector);
+          } else {
+            return null;
+          }
+        } else {
+          return $el.closest(selector);
+        }
+      } else {
+        return null;
+      }
+    }
+    catch (ex) {
+      console.error(ex);
+    }
+
+  }
+
+  // 向前寻找第一个符合selector的兄弟元素
+  function findSiblingForward($el, selector) {
+    if ($el instanceof Element && typeof selector === 'string' && $el.previousElementSibling) {
+      if ($el.previousElementSibling.matches(selector)) {
+        return $el.previousElementSibling;
+      } else {
+        return findSiblingForward($el.previousElementSibling, selector);
+      }
+    } else {
+      return null;
+    }
+  }
+
+  // 向后寻找第一个符合selector的兄弟元素
+  function findSiblingBackward($el, selector) {
+    if ($el instanceof Element && typeof selector === 'string' && $el.nextElementSibling) {
+      if ($el.nextElementSibling.matches(selector)) {
+        return $el.nextElementSibling;
+      } else {
+        return findSiblingBackward($el.nextElementSibling, selector);
+      }
+    } else {
+      return null;
+    }
+  }
+
+  // 寻找第一个符合selector的兄弟元素
+  function findSibling($el, selector, option = 'backward') {
+    try {
+      if (option === 'backward') {
+        return findSiblingBackward($el, selector);
+      } else if (option === 'forward') {
+        return findSiblingForward($el, selector)
+      } else {
+        return null;
+      }
+    }
+    catch (ex) {
+      console.error(ex);
+    }
+  }
+
+  // 寻找符合selector的兄弟元素集合(排除$el本身)
+  function findSiblings($el, selector, option = 'all') {
+    try {
+      if ($el instanceof Element && typeof selector === 'string' && $el.parentElement instanceof Element) {
+        let result = [];
+        if (option === 'all') {
+          let index;
+          result.push(...$el.parentElement.children);
+          console.log(result);
+          index = result.indexOf($el);
+          result.splice(index, 1);  // 移除$el本身
+
+          result = result.filter((el) => {
+            return el.matches(selector);
+          });
+
+          return result;
+        } else if (option === 'backward') {
+          let index;
+          result.push(...$el.parentElement.children);
+          index = result.indexOf($el);
+          result.splice(0, index + 1);  // 从头开始移除sibling，直到把$el也一起移除
+
+          result = result.filter((el) => {
+            return el.matches(selector);
+          });
+
+          return result;
+        } else if (option === 'forward') {
+          let index;
+          result.push(...$el.parentElement.children);
+          index = result.indexOf($el);
+          result.splice(index, result.length - index);  // 从$el开始移除，直到把$el后面的所有sibling全部移除
+
+          result = result.filter((el) => {
+            return el.matches(selector);
+          });
+
+          return result;
         } else {
           return null;
         }
-      } else {
-        return $el.closest(selector);
       }
-    }
-  }
 
-  function findSiblingForward($el, className) {
-    if (typeof $el !== 'undefined' && typeof className !== 'undefined' && $el.previousElementSibling) {
-      if (hasClass($el.previousElementSibling, className)) {
-        return $el.previousElementSibling;
-      } else {
-        return findSiblingForward($el.previousElementSibling, className);
-      }
-    } else {
-      console.log('Such a previous sibling element node is not found');
     }
-  }
-
-  function findSiblingBackward($el, className) {
-    if (typeof $el !== 'undefined' && typeof className !== 'undefined' && $el.nextElementSibling) {
-      if (hasClass($el.nextElementSibling, className)) {
-        return $el.nextElementSibling;
-      } else {
-        return findSiblingBackward($el.nextElementSibling, className);
-      }
-    } else {
-      console.log('Such a next sibling element node is not found');
+    catch (ex) {
+      console.error(ex);
     }
   }
 
@@ -187,8 +308,8 @@ const domOperationModule = (function () {
     query,
     queryAll,
     findClosestAncestor,
-    findSiblingForward,
-    findSiblingBackward
+    findSibling,
+    findSiblings
   }
 })();
 
